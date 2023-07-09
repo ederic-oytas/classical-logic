5250e00745d09333fc1956c4fc219f7e4fb2db0d
Ederic <edericoytas@gmail.com>
2023-07-08 22:17:42 -0700

    Simplify tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,7 @@
 
 from itertools import product
 import pytest
-from typing import Optional
+from typing import Any, Optional
 
 from flogic.core import (
     And,
@@ -28,55 +28,49 @@ simple5: list[Proposition] = [
 simple6: list[Proposition] = [Predicate("p"), *simple5]
 # [p, ~p, p&q, p|q, p->q, p<->q]
 
+P = Predicate("P")
+Q = Predicate("Q")
+
 
 class TestPropositionCompositionMethods:
     """Tests for the five composition methods in the Proposition class."""
 
-    @pytest.mark.parametrize("u", simple6)
+    test_cases: list[Proposition] = [P, Not(P), And(P, Q)]
+
+    @pytest.mark.parametrize("u", test_cases)
     def test_invert(self, u: Proposition):
         assert u.__invert__() == Not(u)
         assert ~u == Not(u)
 
-    binary_test_cases_with_correct_types = [
-        (Predicate("p"), Predicate("q")),  # (p, q)
-        *product(simple5, [Predicate("r")]),  # (~p, r), (p&q, r), ...
-        *product([Predicate("r")], simple5),  # (r, ~p), (r, p&q), ...
-    ]
-    binary_test_cases_with_wrong_second_type = [
-        *product(simple6, [object(), True, False])
-    ]
+    @pytest.mark.parametrize("u", test_cases)
+    def test_and(self, u: Proposition):
+        for v in self.test_cases:
+            assert u.__and__(v) == And(u, v)
+            assert u & v == And(u, v)
+        for x in [object(), True, False]:
+            assert u.__and__(x) == NotImplemented
+            with pytest.raises(TypeError):
+                r = u & x
 
-    @pytest.mark.parametrize("u,v", binary_test_cases_with_correct_types)
-    def test_and_correct_type(self, u: Proposition, v: Proposition):
-        """Tests (u & v) where u and v are both Proposition's"""
-        assert u.__and__(v) == And(u, v)
-        assert u & v == And(u, v)
+    @pytest.mark.parametrize("u", test_cases)
+    def test_or(self, u: Proposition):
+        for v in self.test_cases:
+            assert u.__or__(v) == Or(u, v)
+            assert u | v == Or(u, v)
+        for x in [object(), True, False]:
+            assert u.__or__(x) == NotImplemented
+            with pytest.raises(TypeError):
+                r = u | x
 
-    @pytest.mark.parametrize("u,x", binary_test_cases_with_wrong_second_type)
-    def test_and_incorrect_type(self, u: Proposition, x):
-        """Tests u.__and__(x) where u is a Proposition, but x is not."""
-        assert u.__and__(x) is NotImplemented
+    @pytest.mark.parametrize("u", test_cases)
+    def test_implies(self, u: Proposition):
+        for v in self.test_cases:
+            assert u.implies(v) == Implies(u, v)
 
-    @pytest.mark.parametrize("u,v", binary_test_cases_with_correct_types)
-    def test_or_correct_type(self, u: Proposition, v: Proposition):
-        """Tests (u | v) where u and v are both Proposition's"""
-        assert u.__or__(v) == Or(u, v)
-        assert u | v == Or(u, v)
-
-    @pytest.mark.parametrize("u,x", binary_test_cases_with_wrong_second_type)
-    def test_or_incorrect_type(self, u: Proposition, x):
-        """Tests u.__or__(x) where u is a Proposition, but x is not."""
-        assert u.__or__(x) is NotImplemented
-
-    @pytest.mark.parametrize("u,v", binary_test_cases_with_correct_types)
-    def test_implies(self, u: Proposition, v: Proposition):
-        """Tests u.implies(v) where u and v are both Proposition's"""
-        assert u.implies(v) == Implies(u, v)
-
-    @pytest.mark.parametrize("u,v", binary_test_cases_with_correct_types)
-    def test_iff(self, u: Proposition, v: Proposition):
-        """Tests u.iff(v) where u and v are both Proposition's"""
-        assert u.iff(v) == Iff(u, v)
+    @pytest.mark.parametrize("u", test_cases)
+    def test_iff(self, u: Proposition):
+        for v in self.test_cases:
+            assert u.iff(v) == Iff(u, v)
 
 
 class TestPropositionMiscSpecialMethods:

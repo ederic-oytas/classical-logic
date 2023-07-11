a33bdb6786f047f8af8ff2d346f0d5d58fa330cc
Ederic <edericoytas@gmail.com>
2023-07-11 01:09:27 -0700

    Rework lex tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -19,88 +19,53 @@ from flogic.parsing import (
 
 class TestLex:
     @pytest.mark.parametrize(
-        "s",
+        "text",
         [
             "abcd",
             "sadfmsoerqwkef",
             "\\'\"\n\t\uFFFF",
         ],
     )
-    def test_lex_accept_1(self, s: str):
-        """Tests _lex_accept (part 1)"""
+    def test_lex_accept(self, text: str):
+        """Tests _lex_accept"""
 
-        # Assert that each corresponding character in the string
-        # will be accepted
-        it = iter(s)
-        for c in s:
+        # Every corresponding character should be accepted
+        it = iter(text)
+        for c in text:
             _lex_accept(it, c)
 
+        # Do it again but with wrong chars
+        it = iter(text)
+        for c in text:
+            d = chr(ord(c) + 1)
+            with pytest.raises(ValueError):
+                _lex_accept(it, d)
+
         # Assert that any more characters will not be accepted
-        mes: str = _UNEXP_END_OF_STR
-        with pytest.raises(ValueError, match=re.escape(mes)):
+        with pytest.raises(ValueError):
             _lex_accept(it, "a")
-        with pytest.raises(ValueError, match=re.escape(mes)):
+        with pytest.raises(ValueError):
             _lex_accept(it, "b")
 
     @pytest.mark.parametrize(
-        "s,t",
+        "text,expected_token_type",
         [
-            ("abcd", "efgh"),
-            ("abcd", "hijk"),
+            # Predicate names:
+            ("P", _TokenType.IDENT),
+            (f"_{string.ascii_letters}{string.digits}", _TokenType.IDENT),
+            # Operators:
+            ("~", _TokenType.NOT),
+            ("&", _TokenType.AND),
+            ("|", _TokenType.OR),
+            ("->", _TokenType.IMPLIES),
+            ("<->", _TokenType.IFF),
+            # Separators:
+            ("(", _TokenType.LPARENS),
+            (")", _TokenType.RPARENS),
         ],
     )
-    def test_lex_accept_2(self, s: str, t: str):
-        """Tests _lex_accept (part 2)"""
-
-        # s: String to iterate on
-        # t: Expected characters, which contains incorrect chars
-
-        it = iter(s)
-
-        # Expect that every corresponding character in t is wrong.
-        for c, d in zip(s, t):
-            mes = _unexp_char(c)
-            print(mes)
-            with pytest.raises(ValueError, match=re.escape(mes)):
-                _lex_accept(it, d)
-
-        # Since the iterator is exhausted, expect we get the end of string
-        # error now.
-        for c in s:
-            mes = _UNEXP_END_OF_STR
-            with pytest.raises(ValueError, match=re.escape(mes)):
-                _lex_accept(it, c)
-
-    @pytest.mark.parametrize(
-        "s,expected_token",
-        [
-            ("~", (_TokenType.NOT, "~")),
-            ("&", (_TokenType.AND, "&")),
-            ("|", (_TokenType.OR, "|")),
-            ("->", (_TokenType.IMPLIES, "->")),
-            ("<->", (_TokenType.IFF, "<->")),
-            ("(", (_TokenType.LPARENS, "(")),
-            (")", (_TokenType.RPARENS, ")")),
-        ],
-    )
-    def test_individual_operator_and_separator_tokens(
-        self, s: str, expected_token: tuple[_TokenType, str]
-    ):
-        stream = _lex(s)
-        assert next(stream) == expected_token
-        assert next(stream, None) is None
-
-    @pytest.mark.parametrize(
-        "atomic_name",
-        [
-            "P",
-            string.ascii_letters + string.digits + "_",
-            "_" * 20 + string.ascii_letters + string.digits,
-            string.ascii_uppercase + string.ascii_lowercase,
-        ],
-    )
-    def test_atomic_name_success(self, atomic_name: str):
-        assert list(_lex(atomic_name)) == [(_TokenType.ATOMIC, atomic_name)]
+    def test_lex_single(self, text: str, expected_token_type: _TokenType):
+        assert list(_lex(text)) == [(expected_token_type, text)]
 
     @pytest.mark.parametrize("d", string.digits)
     def test_atomic_name_starting_digit_fail(self, d: str):
@@ -120,18 +85,18 @@ class TestLex:
             (
                 "P \t\f\r\n-> \t\f\r\n Q",
                 [
-                    (_TokenType.ATOMIC, "P"),
+                    (_TokenType.IDENT, "P"),
                     (_TokenType.IMPLIES, "->"),
-                    (_TokenType.ATOMIC, "Q"),
+                    (_TokenType.IDENT, "Q"),
                 ],
             ),
             (
                 "P \t\f\r\n Q R     S",
                 [
-                    (_TokenType.ATOMIC, "P"),
-                    (_TokenType.ATOMIC, "Q"),
-                    (_TokenType.ATOMIC, "R"),
-                    (_TokenType.ATOMIC, "S"),
+                    (_TokenType.IDENT, "P"),
+                    (_TokenType.IDENT, "Q"),
+                    (_TokenType.IDENT, "R"),
+                    (_TokenType.IDENT, "S"),
                 ],
             ),
         ],
@@ -140,16 +105,12 @@ class TestLex:
         """Tests that all whitespace is ignored."""
         assert list(_lex(s)) == expected_tokens
 
-    @pytest.mark.parametrize("c", "0123456789!@#$%^*[]{}\\=")
-    def test_individual_unexpected_characters(self, c: str):
-        """Test for single unexpected characters"""
-        mes = _unexp_char(c)
-        with pytest.raises(ValueError, match=re.escape(mes)):
-            list(_lex(c))
-
     @pytest.mark.parametrize(
-        "s",
+        "text",
         [
+            # Single unexpected character tests
+            *list("0123456789!@#$%^*[]{}\\="),
+            # Tests with unexpected character elsewhere in text
             "-<",
             "--",
             "-P",
@@ -160,25 +121,18 @@ class TestLex:
             "<-<",
             "<-=",
             "<-P",
-        ],
-    )
-    def test_non_first_unexpected_characters(self, s: str):
-        mes = _unexp_char(s[-1])
-        with pytest.raises(ValueError, match=re.escape(mes)):
-            list(_lex(s))
-
-    @pytest.mark.parametrize(
-        "s",
-        [
+            "P Q 0",
+            "P Q 12349",
+            # Tests that unexpectedly reach end of string
             "-",
             "<",
             "<-",
         ],
     )
-    def test_unexpected_end_of_string(self, s: str):
-        mes = _UNEXP_END_OF_STR
-        with pytest.raises(ValueError, match=re.escape(mes)):
-            list(_lex(s))
+    def test_raises_value_error(self, text: str):
+        """Tests that lexing the text raises ValueError"""
+        with pytest.raises(ValueError):
+            list(_lex(text))
 
 
 P = Predicate("P")
